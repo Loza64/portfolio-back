450c9196e6e405a1a09fdc7738b17f10340d6fe1
= <=>
2026-07-11 09:00:49 -0600

    feat: update response messages

--- src/modules/messages/infrastructure/http/message.controller.ts
+++ src/modules/messages/infrastructure/http/message.controller.ts
@@ -2,12 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { CreateMessageUseCase } from 'src/modules/messages/application/create-message.use-case';
 
 export class MessageController {
-  constructor(private readonly createMessageUseCase: CreateMessageUseCase) {}
+  constructor(private readonly createMessageUseCase: CreateMessageUseCase) { }
 
   create = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const message = await this.createMessageUseCase.execute(req.body);
-      res.status(201).json({ message: 'Mensaje enviado correctamente', data: message.toPublic() });
+      res.status(201).json(message.toPublic());
     } catch (err) {
       next(err);
     }
